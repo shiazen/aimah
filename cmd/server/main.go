c7919caedd4275c23d9ef916b4866bbcdb410eca
shiazen <shiazen@yandex.ru>
2022-02-18 17:59:38 +0300

    initial server code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	handler := func(_ http.ResponseWriter, r *http.Request) {
+		// fmt.Printf("Req: %s %s\n", r.Host, r.URL.Path) 
+		fmt.Println(r.URL.Path) 
+	}
+	http.HandleFunc("/", handler)
+
+	http.ListenAndServe(":8080", nil)
+}
