6e508f9ecd4266c69226549ee02488f2de7760e7
shiazen <shiazen@yandex.ru>
2022-03-22 02:57:33 +0300

    agent: json awareness

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,33 +1,47 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"flag"
 	"fmt"
-	"runtime"
-	"net/http"
-	"math/rand"
-	"time"
-	"reflect"
 	"log"
+	"math/rand"
+	"net/http"
 	"os"
 	"os/signal"
+	"reflect"
+	"runtime"
 	"syscall"
+	"time"
 )
 
-var memStats = [26]string{ "Alloc", "BuckHashSys", "Frees",
-		"GCCPUFraction", "GCSys", "HeapAlloc", "HeapIdle",
-		"HeapInuse", "HeapObjects", "HeapReleased", "HeapSys",
-		"LastGC", "Lookups", "MCacheInuse", "MCacheSys",
-		"MSpanInuse", "MSpanSys", "Mallocs", "NextGC",
-		"NumForcedGC", "NumGC", "OtherSys", "PauseTotalNs",
-		"StackInuse", "StackSys", "Sys"}
+var memStats = [27]string{"Alloc", "BuckHashSys", "Frees",
+	"GCCPUFraction", "GCSys", "HeapAlloc", "HeapIdle",
+	"HeapInuse", "HeapObjects", "HeapReleased", "HeapSys",
+	"LastGC", "Lookups", "MCacheInuse", "MCacheSys",
+	"MSpanInuse", "MSpanSys", "Mallocs", "NextGC",
+	"NumForcedGC", "NumGC", "OtherSys", "PauseTotalNs",
+	"StackInuse", "StackSys", "Sys", "RandomValue"}
 
 var ms runtime.MemStats
 var Counter int64
 var RandomValue float64
 
+type Metrics struct {
+	ID    string   `json:"id"`              // имя метрики
+	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
+	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
+	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	client := &http.Client{}
+
+	jsonPtr := flag.Bool("j", true, "talk to server in json")
+	flag.Parse()
+	fmt.Println(*jsonPtr)
 
 	var pollInterval = 2
 	var reportInterval = 10
@@ -42,48 +56,82 @@ func main() {
 	SigChan := make(chan os.Signal, 1)
 	signal.Notify(SigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
-	go func() { }()
+	go func() {}()
 
 	for {
 		select {
-			case datSignal := <-SigChan:
+		case datSignal := <-SigChan:
 			switch datSignal {
-				case syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
-					fmt.Printf("\nSignal %v triggered.\n", datSignal)
-					os.Exit(0)
-				default:
-					fmt.Printf("\nSignal %v triggered.\n", datSignal)
-					os.Exit(1)
+			case syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+				fmt.Printf("\nSignal %v triggered.\n", datSignal)
+				os.Exit(0)
+			default:
+				fmt.Printf("\nSignal %v triggered.\n", datSignal)
+				os.Exit(1)
 			}
-			case <-TickerPoll.C:
-				runtime.ReadMemStats(&ms)
-				RandomValue = rand.Float64()
-				Counter++
-			case <-TickerSend.C:
-				//fmt.Println(Counter, RandomValue)
-				for i, varName := range memStats {
-					varValue := getField(&ms, memStats[i])
-					varType := "gauge"
-					query := fmt.Sprintf("%v:%v/update/%v/%v/%v", serverAddress, serverPort, varType, varName, varValue)
-					sendStuff(client, query)
+		case <-TickerPoll.C:
+			runtime.ReadMemStats(&ms)
+			RandomValue = rand.Float64()
+			Counter++
+		case <-TickerSend.C:
+			var query string
+			var Payload []byte
+
+			for i, varName := range memStats {
+				varValue := getField(&ms, memStats[i])
+				varType := "gauge"
+				if *jsonPtr {
+					Payload = jsonify(Metrics{ID: varName, MType: varType, Value: &varValue})
+					query = fmt.Sprintf("%v:%v/update/", serverAddress, serverPort)
+					fmt.Println(string(Payload))
+					sendStuff(client, query, Payload, "application/json")
+				} else {
+					query = fmt.Sprintf("%v:%v/update/%v/%v/%v", serverAddress, serverPort, varType, varName, varValue)
+					sendStuff(client, query, Payload, "text/plain")
 				}
-				sendStuff(client, fmt.Sprintf("%v:%v/update/%v/%v/%v", serverAddress, serverPort, "gauge", "RandomValue", RandomValue))
-				sendStuff(client, fmt.Sprintf("%v:%v/update/%v/%v/%v", serverAddress, serverPort, "counter", "PollCount", Counter))
+			}
+			if *jsonPtr {
+				Payload = jsonify(Metrics{ID: "PollCount", MType: "counter", Delta: &Counter})
+				fmt.Println(string(Payload))
+				query = fmt.Sprintf("%v:%v/update/", serverAddress, serverPort)
+				sendStuff(client, query, Payload, "application/json")
+			} else {
+				query = fmt.Sprintf("%v:%v/update/%v/%v/%v", serverAddress, serverPort, "counter", "PollCount", Counter)
+				sendStuff(client, query, Payload, "text/plain")
+			}
 		}
 	}
 }
 
-func getField(v *runtime.MemStats, field string) reflect.Value {
+func getField(v *runtime.MemStats, field string) float64 {
 	r := reflect.ValueOf(v)
 	f := reflect.Indirect(r).FieldByName(field)
-	return f
+	if field == "GCCPUFraction" {
+		return (f.Float())
+	} else if field == "RandomValue" {
+		return RandomValue
+	}
+	return float64(f.Uint())
 }
 
-func sendStuff (c *http.Client, q string) {
-	request, err := http.NewRequest(http.MethodPost, q, nil)
-	if err != nil { log.Fatal(err) }
-	request.Header.Set("Content-Type", "text/plain")
+func sendStuff(c *http.Client, q string, b []byte, h string) {
+	request, err := http.NewRequest(http.MethodPost, q, bytes.NewReader(b))
+	if err != nil {
+		log.Fatal(err)
+	}
+	request.Header.Set("Content-Type", h)
+	//request.Header.Set("Content-Type", "application/json")
 	resp, err := c.Do(request)
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	resp.Body.Close()
+}
+
+func jsonify(m Metrics) []byte {
+	p, err := json.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return p
 }
